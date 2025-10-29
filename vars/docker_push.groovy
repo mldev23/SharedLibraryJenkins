a8b5aba03cadbd9029325233f45327ff1626dfcc
mldev23 <ronalcris407@gmail.com>
2025-10-29 22:23:14 +0530

    Fix Docker login and push commands to use env variables

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,7 @@
 def call(String Project,String ImageTag,String DockerHubUser){
   withCredentials([usernamePassword(credentialsId:'dockerHubCred',passwordVariable:'dockerhubpass',usernameVariable:'dockerhubuser')]){
-  sh "docker login -u ${dockerhubuser}" -p ${dockerhubpass}"
+  sh "docker login -u ${env.dockerhubuser} -p ${env.dockerhubpass}"
   }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+  sh "docker push ${env.dockerhubuser}/${Project}:${ImageTag}"
   
 }

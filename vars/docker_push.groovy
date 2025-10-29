9c3007c0965395870fe566b9e33a35f4ec5ea8fb
mldev23 <ronalcris407@gmail.com>
2025-10-29 23:31:03 +0530

    Refactor docker_push to improve security and feedback
    Updated docker_push function to use password-stdin for login and added success message after pushing the image.

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,9 @@
-def call(String Project,String ImageTag,String DockerHubUser){
-  withCredentials([usernamePassword(credentialsId:'dockerHubCred',passwordVariable:'dockerhubpass',usernameVariable:'dockerhubuser')]){
-  sh "docker login -u ${env.dockerhubuser} -p ${env.dockerhubpass}"
-  }
-  sh "docker push ${DockerHubUser}/${Project}:${ImageTag}"
-  
+def call(String Project, String ImageTag, String DockerHubUser) {
+    withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
+        sh """
+            echo $dockerhubpass | docker login -u $dockerhubuser --password-stdin
+            docker push ${DockerHubUser}/${Project}:${ImageTag}
+        """
+    }
+    echo "Docker image pushed successfully: ${DockerHubUser}/${Project}:${ImageTag}"
 }

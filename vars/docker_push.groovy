cfd1701791ca811fe3ef7025161e98b7a6623b3a
mldev23 <ronalcris407@gmail.com>
2025-10-29 23:29:06 +0530

    Fix Docker push command to use DockerHubUser

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,6 +2,6 @@ def call(String Project,String ImageTag,String DockerHubUser){
   withCredentials([usernamePassword(credentialsId:'dockerHubCred',passwordVariable:'dockerhubpass',usernameVariable:'dockerhubuser')]){
   sh "docker login -u ${env.dockerhubuser} -p ${env.dockerhubpass}"
   }
-  sh "docker push ${env.dockerhubuser}/${Project}:${ImageTag}"
+  sh "docker push ${DockerHubUser}/${Project}:${ImageTag}"
   
 }
